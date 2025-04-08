d93ddd9bc0ccdd0b4c39e13ccd0d680589dbb1e6
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-04-08 13:55:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.java
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.java
@@ -6,45 +6,39 @@ import java.util.*;
 // } Driver Code Ends
 
 class Solution {
-
     public ArrayList<Integer> jobSequencing(int[] d, int[] p) {
-       int n = d.length;
-        Job[] arr = new Job[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = new Job(d[i], p[i]);
+        // code here
+        ArrayList<int []>a=new ArrayList<>();
+        int n=d.length ,ans=0 ,t=0;
+        
+        for(int i=0;i<d.length;i++){
+            a.add(new int[]{d[i],p[i]});
         }
+        Collections.sort(a,(x,y)->y[1]-x[1]);
         
-        Arrays.sort(arr, (a, b) -> b.profit - a.profit);
+        int max=0;
+        for(int i:d)max=Math.max(i,max);
         
-        int maxJobDeadline = 0;
-        for (Job a : arr) {
-            maxJobDeadline = Math.max(maxJobDeadline, a.deadline);
-        }
+        boolean arr[] = new boolean[max + 1];
         
-        int[] fillTime = new int[maxJobDeadline];
-        Arrays.fill(fillTime, -1);
-        
-        int profit = 0, jobsDone = 0;
-        for (Job a : arr) {
-            for (int i = a.deadline - 1; i >= 0; i--) {
-                if (fillTime[i] == -1) {
-                    fillTime[i] = i;
-                    profit += a.profit;
-                    jobsDone++;
+        for(int i1[]:a){
+            int x=i1[0];
+            int y=i1[1];
+            
+            for(int i=x;i>0;i--){
+                if(arr[i]==false){
+                    arr[i]=true;
+                    t++;
+                    ans+=y;
                     break;
                 }
             }
         }
+        ArrayList<Integer>ret=new ArrayList<>();
+        ret.add(t);
+        ret.add(ans);
         
-        return new ArrayList<>(Arrays.asList(jobsDone, profit));
-    }
-
-    class Job {
-        int deadline, profit;
-        Job(int deadline, int profit) {
-            this.deadline = deadline;
-            this.profit = profit;
-        }
+        return ret;
     }
 }
 
