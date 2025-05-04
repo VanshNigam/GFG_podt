68d898c82ccd887e1b290416d7e73049c419cd0d
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-05-04 10:09:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest distinct window/smallest-distinct-window.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String str = br.readLine();
+
+            Solution obj = new Solution();
+            System.out.println(obj.findSubString(str));
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findSubString(String str) {
+        // code here
+        
+    }
+}
