c2f0c5d71b8d0354a758f9048de01d4175fdd4dc
Vansh nigam <151783953+VanshNigam@users.noreply.github.com>
2025-07-29 20:03:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorting a Numpy Array/sorting-a-numpy-array.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def sort_array(arr):
+
+    arr = np.array(arr)
+    
+    # Use NumPy's sort function to sort the array
+    sorted_arr = np.sort(arr)
+    
+    return sorted_arr
